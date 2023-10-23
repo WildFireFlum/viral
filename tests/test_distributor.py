de35ffcf49e8e98411977cbd1fcba8c96d6befac
tzlilgonen <tzlilgon@gmail.com>
2023-10-23 20:00:56 +0300

    Introduce Template

--- tests/test_distributor.py
+++ tests/test_distributor.py
@@ -1,27 +1,39 @@
+from io import StringIO
 from pathlib import Path
 
 from pytest import fixture
 
+from viral.core.distributor import Distributor, Template
 from viral.core.generator import Statement
-from viral.examples.cppgen import ExampleCppGenerator
-from viral.examples.pygen import ExamplePythonGenerator
-from viral.core.distributor import Distributor
-
 
 
 @fixture
-def distributor():
+def statement():
+    return Statement(data='watwat = wat wat wat')
+
+
+@fixture
+def template():
+    return Template(header='header', footer='footer', indentation='indentation')
+
+
+@fixture
+def distributor(statement: Statement, template: Template) -> Distributor:
     output_file = Path(__file__).parent / 'example_file'
     dist = Distributor()
-    dist.add_statement(output_file, Statement(data='watwat = wat wat wat'))
+    dist.add_statement(output_file, statement)
+    dist.add_template(output_file, template)
     yield dist
     output_file.unlink(missing_ok=True)
 
 
 class TestDistributor:
 
-    def test_example_valid_distribution(self, distributor):
-        config_files = distributor.config.keys()
+    def test_example_valid_distribution(self, distributor: Distributor, statement: Statement, template: Template):
+        assert any((statement in s) for s in distributor.statements.values())
+        assert template in distributor.templates.values()
+
+        config_files = distributor.statements.keys()
         distributor.distribute()
 
         assert config_files
@@ -29,3 +41,23 @@ class TestDistributor:
             assert path.exists()
             assert path.stat().st_size > 0
 
+    def test_default_template_dump(self, statement: Statement):
+        template = Template()
+        for copies in range(2):
+            string_io = StringIO()
+            template.dump(string_io, [statement] * copies)
+            output = string_io.getvalue()
+            assert output.count(statement.data) == copies
+            assert output.count('\n') == copies
+
+    def test_template_dump(self, template: Template, statement: Statement):
+        string_io = StringIO()
+        copies = 3
+        statements = [statement] * copies
+        template.dump(string_io, statements)
+        output = string_io.getvalue()
+        assert output.count(template.header) == 1
+        assert output.count(template.footer) == 1
+        assert output.count(template.indentation) == copies
+        assert output.count(f'{template.indentation}{statement.data}') == copies
+        assert output.count('\n') == 1 + copies + 1
--- viral/core/distributor.py
+++ viral/core/distributor.py
@@ -1,29 +1,55 @@
 from collections import defaultdict
 from copy import copy
+from dataclasses import dataclass
 from pathlib import Path
-from typing import Dict, Set
+from typing import Dict, TextIO, Set, Collection
+
 from viral.core.generator import Statement
+
+
+@dataclass(frozen=True)
+class Template:
+    header: str = ''
+    footer: str = ''
+    indentation: str = ''
+
+    def dump(self, text_io: TextIO, statements: Collection[Statement]) -> None:
+        if self.header:
+            text_io.write(self.header)
+            text_io.write('\n')
+        if statements:
+            sorted_statements = sorted(f'{self.indentation}{statement.data}' for statement in statements)
+            text_io.write('\n'.join(sorted_statements))
+            text_io.write('\n')
+        if self.footer:
+            text_io.write(self.footer)
+            text_io.write('\n')
 
 
 class Distributor:
 
     def __init__(self):
-        self._config: Dict[Path, Set[Statement]] = defaultdict(lambda: set())
+        self._statements: Dict[Path, Set[Statement]] = defaultdict(set)
+        self._templates: Dict[Path, Template] = defaultdict(Template)
 
     @property
-    def config(self):
-        # TOOD: deep copy
-        return copy(self._config)
+    def statements(self):
+        # TODO: deep copy
+        return copy(self._statements)
 
-    def add_statement(self, path: Path, statement: Statement):
-        self._config[path].add(statement)
+    @property
+    def templates(self):
+        # TODO: deep copy
+        return copy(self._templates)
 
-    ## TODO in a million years - distribute using multiprocessing
+    def add_template(self, path: Path, template: Template) -> None:
+        self._templates[path] = template
+
+    def add_statement(self, path: Path, statement: Statement) -> None:
+        self._statements[path].add(statement)
+
+    # TODO in a million years - distribute using multiprocessing
     def distribute(self) -> None:
-        for file, statements in self._config.items():
-            sorted_statements = sorted(statement.data for statement in statements)
-            file.write_text('\n'.join(sorted_statements))
-
-
-
-
+        for path, statements in self._statements.items():
+            with path.open('w+') as f:
+                self._templates[path].dump(f, statements)
